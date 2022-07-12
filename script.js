624f6eaac0f59764d685c00bf098429ee3f41485
RomuloBrumDev <romulobrum28@gmail.com>
2022-07-11 23:53:37 -0300

    iniciando o desafio

---
+++ script.js
@@ -0,0 +1,26 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+for (var i = 1; i <= 10; i++) {
+  console.log(i);
+}
+var i = 10;
+
+console.log("-------------------------------------------------------------");
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+var c = 11;
+
+while (c >= 2) {
+  c--;
+  console.log(c);
+}
+
+console.log("-------------------------------------------------------------");
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+for (var i = 1; i >= 100; i++) {
+  console.log(i);
+}
+var i = 1;
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
